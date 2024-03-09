81fb079334d399910d9f95837a5d63633b5c0cb6
Dongwook Kim <ehddnr9797@gmail.com>
2024-03-10 03:30:14 +0900

    chore: minor fix

--- src/test/java/com/kimchipremium/kimchipremium/services/UpbitServiceTest.java
+++ src/test/java/com/kimchipremium/kimchipremium/services/UpbitServiceTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UpbitServiceTest {
 
-    private UpbitService upbitService;
+    UpbitService upbitService;
 
     @BeforeEach
     void beforeEach() {
